6e65983f55d8883ac19b4f758e819d863a2c0fd6
JML <a>
2026-02-26 11:07:43 +0100

    Versión 3: cálculo de impuestos

--- src/com/empresa/ventas/Main.java
+++ src/com/empresa/ventas/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Producto p = new Producto("Zapatillas", 50);
         Venta v = new Venta();
-        System.out.println("Total: " + v.calcularTotal(p, 2));
+        System.out.println("Total con impuestos: " +
+                v.calcularTotal(p, 2) * 1.21);
+
     }
 }
--- src/com/empresa/ventas/Venta.java
+++ src/com/empresa/ventas/Venta.java
@@ -4,4 +4,8 @@ public class Venta {
     public double calcularTotal(Producto producto, int cantidad) {
         return producto.getPrecio() * cantidad;
     }
+    public double calcularTotalConImpuesto(Producto producto, int cantidad, double impuesto) {
+        double total = calcularTotal(producto, cantidad);
+        return total + (total * impuesto);
+    }
 }
