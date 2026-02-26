853210aa94b02f064840f4bad8ae31773b85067c
JML <a>
2026-02-26 10:43:01 +0100

    1.2 impuesto añadido

--- src/com/empresa/ventas/Venta.java
+++ src/com/empresa/ventas/Venta.java
@@ -4,10 +4,6 @@ public class Venta {
     public double calcularTotal(Producto producto, int cantidad) {
         return producto.getPrecio() * cantidad;
     }
-    public double calcularTotalConDescuento(Producto producto, int cantidad, double descuento) {
-        double total = calcularTotal(producto, cantidad);
-        return total - (total * descuento);
-    }
     public double calcularTotalConImpuesto(Producto producto, int cantidad, double impuesto) {
         double total = calcularTotal(producto, cantidad);
         return total + (total * impuesto);
